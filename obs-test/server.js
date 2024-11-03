16d8bc98684818181ddc2a928c02e3493c42cca1
gominzip <ahaminji@naver.com>
2024-11-04 00:06:41 +0900

    feat: HLS 변환 적용

--- obs-test/server.js
+++ obs-test/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("path");
 const NodeMediaServer = require("node-media-server");
 
+const app = express();
 const config = {
   rtmp: {
     port: 1935,
@@ -13,13 +14,27 @@ const config = {
   http: {
     port: 8000,
     allow_origin: "*",
+    mediaroot: path.join(__dirname, "media"), // HLS 파일 저장 경로
+  },
+  trans: {
+    ffmpeg: "/opt/homebrew/bin/ffmpeg", // FFmpeg 설치 경로로 변경
+    tasks: [
+      {
+        app: "live",
+        ac: "aac", // 오디오 코덱
+        vc: "libx264", // 비디오 코덱
+        hls: true, // HLS 출력 설정
+        hlsFlags: "[hls_time=1:hls_list_size=10:hls_flags=delete_segments]", // HLS 옵션
+      },
+    ],
   },
 };
 
+// NodeMediaServer 실행
 const nms = new NodeMediaServer(config);
 nms.run();
 
-const app = express();
+// 정적 파일 제공
 app.use(express.static(path.join(__dirname, "public")));
 
 app.listen(3000, () => {
